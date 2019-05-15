b2032dbe42eaa5dd2893d3e55afbcfbe0badb4ca
Pasha_HOME <dvoynospavel@gmail.com>
2019-05-15 14:19:38 +0300

    del a comments

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -13,14 +12,11 @@ const port = process.env.PORT || 5000;
 mongoose.connect(process.env.DB_CONNECTION,
 	{useNewUrlParser: true}, () => console.log('Connected to DB'));
 
+const app = express();
 app.use(cors());
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use('/api', tasks);
-
 app.listen(port, () => {
 	 console.log(`Server start on port ${port}`)
 });
